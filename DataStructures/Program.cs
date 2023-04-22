bb8af1ef7105bf3549ac32b486967c8a5834385d
Chandler McAdory <chandlermcadory@gmail.com>
2023-04-22 19:05:53 -0400

    binary search

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter Array Length");
+string value  = Console.ReadLine();
+int arrayLength = Convert.ToInt32(value);
+int[] arrayName01= new int[arrayLength];
+
+int counter = 0;
+
+//while(counter <= arrayName01.Length)
+//{
+//    Console.WriteLine(counter);
+//    counter++;
+//}
+//Console.WriteLine("Press any key to continue.");
+//Console.ReadKey();
+
+int offSet = new Random().Next(1000);
+
+for (; counter < arrayName01.Length; counter++)
+{
+    arrayName01[counter] = offSet + counter;
+    //Console.WriteLine(arrayName01[counter]);
+}
+
+
+void binarySearch(int numberToFind)
+{
+    int min = 0;
+    int max = arrayLength - 1;
+    int midPoint = (arrayLength - 1) / 2;
+
+    while(arrayName01[midPoint] != numberToFind)
+    {
+        if(arrayName01[midPoint] < numberToFind)
+        {
+            min = midPoint + 1;
+        }
+        if(arrayName01[midPoint] > numberToFind)
+        {
+            max = midPoint - 1;
+        }
+        midPoint = (max + min) / 2;
+    }
+    Console.WriteLine("Number found at location: " + midPoint);
+}
+
+binarySearch(new Random().Next(offSet, arrayLength + offSet));
+
+Console.WriteLine("Press any key to continue.");
+Console.ReadKey();
